3c164154f93adaf23533951df37f59811fdaf631
Alex Wilmer <djazium@gmail.com>
2020-03-03 13:36:46 -0500

    ✨ tmp html file to fax

--- form/index.js
+++ form/index.js
@@ -4,28 +4,38 @@ import { render } from 'react-dom'
 let fnsFolder = '.netlify/functions'
 
 let Form = () => {
-  let [text, setText] = useState('')
+  let [ownerName, setOwnerName] = useState('')
+  let [faxNum, setFaxNum] = useState('')
+
+  let sendFax = async () => {
+    let response = await fetch(`${fnsFolder}/sendfax`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ownerName, faxNum
+      })
+    }).then(r => r.text())
+      .catch(error => {
+        console.log('error', error.message)
+      })
+    console.log(response)
+  }
 
   return (
     <>
-      <input value={text} onChange={e => setText(e.target.value)} />
-      <button
-        onClick={async () => {
-          let response = await fetch(`${fnsFolder}/sendfax`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              foo: 'test123'
-            })
-          }).then(r => r.text())
-            .catch(error => {
-              console.log('error', error.message)
-            })
-          console.log(response)
-        }}
-      >send fax</button>
+      <h2>Prescription Approval for Pawprint Oxygen</h2>
+      <label>Owner Name</label>
+      <input value={ownerName} onChange={e => setOwnerName(e.target.value)} />
+
+      <label>Fax Number</label>
+      <input value={faxNum} onChange={e => setFaxNum(e.target.value)} />
+      <div>
+        <button
+          onClick={sendFax}
+        >send fax</button>
+      </div>
     </>
   )
 }
--- functions/sendfax.js
+++ functions/sendfax.js
@@ -1,4 +1,5 @@
 exports.handler = async (event, context) => {
+  require('dotenv').config()
 
   /*
   this works, keep this as an exmaple
@@ -9,31 +10,40 @@ exports.handler = async (event, context) => {
   });
   */
 
-  require('dotenv').config()
   let querystring = require('querystring')
   let Phaxio = require('phaxio-official')
+  let uuid = require('uuid')
+  let fs = require('fs')
 
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  const params = querystring.parse(event.body);
-  const to = '+18556993366'
-  const phaxio = new Phaxio(process.env.PHAXIOKEY, process.env.PHAXIOSECRET);
+  let { ownerName, faxNum } = params
+
+  let params = querystring.parse(event.body);
+  let phaxio = new Phaxio(process.env.PHAXIOKEY, process.env.PHAXIOSECRET);
+
+  let path = __dirname + '/fax-' + uuid.v4() + '.html'
+
+  fs.writeFileSync(path,
+    `<div>Owner Name: ${ownerName}</div>`
+  );
+
 
   // Send a single fax containing two documents: one a URL, one from the filesystem.
   return phaxio.faxes.create({
-    // to: process.env.FAXNUM, // Replace this with a number that can receive faxes.
-    to,
-    content_url: 'https://pawprintoxygen.com/',
+    to: faxNum,
+    file: path,
   })
     .then(fax => {
+      fs.unlinkSync(path)
       // The `create` method returns a fax object with methods attached to it for doing things
       // like cancelling, resending, getting info, etc.
 
       // Wait 5 seconds to let the fax send, then get the status of the fax by getting its info from the API.
-      return new Promise(r => setTimeout(() => {
-        r(fax.getInfo())
+      return new Promise(resolve => setTimeout(() => {
+        resolve(fax.getInfo())
       }, 5000))
     })
     .then(status => {
@@ -43,6 +53,8 @@ exports.handler = async (event, context) => {
       };
     })
     .catch(err => {
+      fs.unlinkSync(path)
+
       return {
         statusCode: 500,
         body: `something broke ${JSON.stringify(err.message, null, 2)} testing! ${params.foo}`
